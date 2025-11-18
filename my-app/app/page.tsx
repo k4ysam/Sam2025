2890f23d53835dfa2ff2c889bc69e94d231d7433
Sam <ksamaksh0601@gmail.com>
2025-11-18 18:31:00 -0500

    revamp and move to hackathon frontend

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,17 @@
 import ResumePage from "@/components/ui/portfolio-hero-with-paper-shaders";
+import Home from "@/components/ui/hero-ascii";
 
 export default function DemoOne() {
   return (
     <div className="min-h-screen h-full w-full">
-    <ResumePage/>
+      <Home />
+
     </div>
   );
 }
+
+
+
+
+
+
